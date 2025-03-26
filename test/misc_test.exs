de87915a3fcc847a50e58969d87f373052682a15
Brian Underwood <public@brian-underwood.codes>
2025-03-26 14:29:37 +0100

    chore: Add some misc cases to make sure we're not getting false positives

---
+++ test/misc_test.exs
@@ -0,0 +1,71 @@
+defmodule CredoUnneccesaryReduce.MiscTest do
+  use Credo.Test.Case, async: true
+
+  alias CredoUnneccesaryReduce.Check
+
+  # Don't really know that this would happen, TBH, because it means
+  # you're doing a calculation that doesn't have any result, but good to check
+  # that it doesn't return false positives here
+  test "important part isn't the return value" do
+    """
+    defmodule NeoWeb.TestModule do
+      def mult(numbers) do
+        Enum.reduce(numbers, [], fn i, acc ->
+          intermediate = i / 3
+
+          side_effect_fn(intermediate)
+
+          acc ++ [intermediate * 2]
+
+          acc
+        end)
+      end
+    end
+    """
+    |> to_source_file("lib/neo_web/test_module.ex")
+    |> run_check(Check)
+    |> refute_issues()
+
+    """
+    defmodule NeoWeb.TestModule do
+      def mult(numbers) do
+        Enum.reduce(numbers, [], fn i, acc ->
+          [i * 2 | acc]
+
+          acc * 2
+        end)
+      end
+    end
+    """
+    |> to_source_file("lib/neo_web/test_module.ex")
+    |> run_check(Check)
+    |> refute_issues()
+
+    """
+    defmodule NeoWeb.TestModule do
+      def mult(numbers) do
+        Enum.reduce(numbers, [], fn i, acc ->
+          if rem(number, 2) == 0 do
+            [number | result]
+          else
+            result
+          end
+
+          i * 5
+        end)
+      end
+    end
+    """
+    |> to_source_file("lib/neo_web/test_module.ex")
+    |> run_check(Check)
+    |> refute_issues()
+  end
+
+  def assert_check_issue(code, message) do
+    code
+    |> assert_issue(fn issue ->
+      assert issue.message == message
+      assert issue.category == :refactor
+    end)
+  end
+end
